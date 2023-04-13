f3b2da10fc51555e709b142d20f85f306c4fdf6c
Ravi Kant <75370682+topdogfound@users.noreply.github.com>
2023-04-13 17:13:20 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "<pre>";
+if($_SERVER['REQUEST_URI'] == '/')
+{
+    echo "Home";
+}
+else if($_SERVER["REQUEST_URI"] == "/raviKant")
+{
+    echo "Ravi Kant";
+}
+else if($_SERVER['REQUEST_URI'] == "/ritika")
+{
+    echo "Ritika";
+}
+else if($_SERVER['REQUEST_URI'] == '/yugalSharma')
+{
+    echo "Yugal Sharma";
+}
